268f3e62bdb2f134aea699055f8a44f1f64715d8
raymag <prayk11@outlook.com>
2021-02-06 16:28:52 -0300

    feat: add sign up mechanism using firebase auth

--- app/src/main/java/com/raymag/kchat/LoginActivity.kt
+++ app/src/main/java/com/raymag/kchat/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -11,12 +12,20 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        btn_sign_in.setOnClickListener {signIn()}
+        btn_sign_in.setOnClickListener {signIn(
+            email_sign_in.text.toString(),
+            password_sign_in.text.toString()
+        )}
         no_account.setOnClickListener {noAccount()}
     }
 
-    private fun signIn() {
+    private fun signIn(email: String, password: String) {
         Toast.makeText(this, "Para MATEEEUS!!", Toast.LENGTH_SHORT).show()
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+
+            }
     }
 
     private fun noAccount() {
--- app/src/main/java/com/raymag/kchat/MainActivity.kt
+++ app/src/main/java/com/raymag/kchat/MainActivity.kt
@@ -3,7 +3,9 @@ package com.raymag.kchat
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +13,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_sign_up.setOnClickListener {signUp()}
+        btn_sign_up.setOnClickListener {signUp(
+            email_sign_up.text.toString(),
+            password_sign_up.text.toString(),
+            username_sign_up.text.toString()
+        )}
         already_own_account.setOnClickListener {goToLoginActivity()}
     }
 
-    private fun signUp() {
-        val username = username_sign_up.text.toString()
-        Toast.makeText(this, "Ai $username!", Toast.LENGTH_SHORT).show()
+    private fun signUp(email: String, password: String, username: String) {
+        if (email.isEmpty() || password.isEmpty() || username.isEmpty()) {
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+        Toast.makeText(this, "Ai ${username}!", Toast.LENGTH_SHORT).show()
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun goToLoginActivity() {
